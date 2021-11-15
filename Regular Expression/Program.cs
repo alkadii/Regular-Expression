26374882d141a3772efbcdc4d9eae96c4e0f95ee
khaled <alkadi.khaled@hotmail.com>
2021-11-15 15:46:39 +0100

    11

---
+++ Regular Expression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Regular_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ملاحظة هامة
+            //Regular_Expression (using System.Text.RegularExpressions;) 
+            //  Regex المتغير من نوع
+
+            #region \d:     أي رقم عدد(1) من صفر ل 9
+            #endregion
+            #region \w      أي حرف كبير أو صغير أو رقم
+            #endregion
+            #region \s      مسافة
+            #endregion
+            #region []      تستخدم في التنسيق
+            #endregion
+            #region {}       تستخدم لعدد الخانات
+            #endregion
+            #region *        تستخدم في الخانات (يمكن للمستخدم أن يدخل خانات أو لا يدخل أبدا) إلخ
+            #endregion
+            #region          تستخدم في الخانات (يمكن للمستخدم ادخال خانات ولكن على الاقل خانة واحدة) الخ
+            #endregion
+            #region ?        إدخال خانة فقط أو عدم الادخال
+            #endregion
+            #region()        تستخدم للتجميع
+            #endregion
+            #region |        أو
+            #endregion
+            #region \+, \- ...        اعطاء المستخدم امكانية إدخال رمز من رموز 
+            #endregion
+            #region .        تجاهل الخانة
+            #endregion
+        }
+    }
+}
